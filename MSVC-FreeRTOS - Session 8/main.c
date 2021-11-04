d1a3cd1fdbf0463259e180e20368276895248ece
Michel Sommer <skruen@gmail.com>
2021-11-04 14:25:17 +0100

    session 8

---
+++ MSVC-FreeRTOS - Session 8/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Priorities at which the tasks are created. */
+#define TASK_MY_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
+#define	TASK_MY_SECOND_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define	TASK_MY_THIRD_TASK_PRIORITY	( tskIDLE_PRIORITY + 3 )
+/* Task stack sizes*/
+#define TASK_MY_TASK_STACK				( configMINIMAL_STACK_SIZE )
+#define	TASK_MY_SECOND_TASK_STACK		( configMINIMAL_STACK_SIZE )
+#define	TASK_MY_THIRD_TASK_STACK		( configMINIMAL_STACK_SIZE )
+
+/* Task Handles */
+TaskHandle_t _taskSecondHandle = NULL;
+
+//Semaphore/Mutex
+// xSemaphoreHandle xSemaphoreMutex;
+
+
+
+
+// --------------------------------------------------------------------------------------
+void taskMyTask(void* pvParameters)
+{
+//	xSemaphoreMutex = xSemaphoreCreateMutex();
+	// Remove compiler warnings.
+	(void)pvParameters;
+
+//	if (xSemaphoreMutex == NULL) {
+		for (;;)
+		{
+			vTaskDelay(pdMS_TO_TICKS(200));
+			puts("Hi from My Task 1");
+		}
+	//}
+//	free(xSemaphoreMutex);
+}
+
+// --------------------------------------------------------------------------------------
+void taskMySeccondTask(void* pvParameters)
+{
+//	xSemaphoreMutex = xSemaphoreCreateMutex();
+	// Remove compiler warnings.
+	(void)pvParameters;
+
+//	if (xSemaphoreMutex == NULL) {
+		for (;;)
+		{
+			vTaskDelay(pdMS_TO_TICKS(200));
+			puts("Hi from My Task 2");
+		}
+	//}
+//free(xSemaphoreMutex);
+}
+
+// --------------------------------------------------------------------------------------
+void taskMyThirdTask(void* pvParameters)
+{
+//	xSemaphoreMutex = xSemaphoreCreateMutex();
+	// Remove compiler warnings.
+	(void)pvParameters;
+
+//	if (xSemaphoreMutex == NULL) {
+		for (;;)
+		{
+			vTaskDelay(pdMS_TO_TICKS(200));
+			puts("Hi from My Task 3");
+		}
+//	}
+//	free(xSemaphoreMutex);
+}
+
+// --------------------------------------------------------------------------------------
+void main(void)
+{
+	/* Create the task, not storing the handle. */
+	xTaskCreate(
+		taskMyTask,       /* Function that implements the task. */
+		"MyTask",          /* Text name for the task. */
+		TASK_MY_TASK_STACK,      /* Stack size in words, not bytes. */
+		(void*)1,    /* Parameter passed into the task. */
+		TASK_MY_TASK_PRIORITY,/* Priority at which the task is created. */
+		NULL);      /* Used to pass out the created task's handle. */
+
+		/* Create the task, storing the handle. */
+	xTaskCreate(
+		taskMySeccondTask,       /* Function that implements the task. */
+		"MySecondTask",          /* Text name for the task. */
+		TASK_MY_SECOND_TASK_STACK,      /* Stack size in words, not bytes. */
+		(void*)2,    /* Parameter passed into the task. */
+		TASK_MY_SECOND_TASK_PRIORITY,/* Priority at which the task is created. */
+		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
+
+		/* Create the task, storing the handle. */
+	xTaskCreate(
+		taskMyThirdTask,       /* Function that implements the task. */
+		"MyThirdTask",          /* Text name for the task. */
+		TASK_MY_THIRD_TASK_STACK,      /* Stack size in words, not bytes. */
+		(void*)2,    /* Parameter passed into the task. */
+		TASK_MY_THIRD_TASK_PRIORITY,/* Priority at which the task is created. */
+		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
+
+	// Let the operating system take over :)
+	vTaskStartScheduler();
+}
