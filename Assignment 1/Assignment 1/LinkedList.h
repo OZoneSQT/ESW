27d6faf1be727cf51af181fd8f954ff40d0a625f
Michel Sommer <skruen@gmail.com>
2021-11-11 11:05:17 +0100

    session 9
    create default freeRtos project

--- Assignment 1/Assignment 1/LinkedList.h
+++ Assignment 1/Assignment 1/LinkedList.h
@@ -2,12 +2,28 @@
 #include <stdint.h>
 
 enum ListReturnCode {
-    OK,
+    OK = 0,
     EMPTY,
     NOT_FOUND,
     NULL,
     ERROR,
 };
+
+typedef struct Item* item 
+{
+    // Anonymous union
+    union {
+        void* data_a;
+        void* data_b;
+    };
+}
+
+typedef struct Node* node
+{
+    void* item;
+    uint16_t index;
+    Node* next;
+}
 
 void create();
 ListReturnCode destroy();
